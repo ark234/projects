e8e183a2539aadc394caf6b9d3c75a1f29a18992
Arkadiy Nigay <anigay@deloitte.com>
2017-11-29 05:46:26 -0500

    adding fade out animation at start to show level. added cursor to body

--- Final_Project_17_Arkadiy_Nigay/js/hover.js
+++ Final_Project_17_Arkadiy_Nigay/js/hover.js
@@ -2,7 +2,7 @@ var modalWin = document.getElementById("modal-win");
 var modalLose = document.getElementById("modal-lose");
 var explosion = document.getElementById("explosion");
 var fireworks = document.getElementById("fireworks");
-var container = document.getElementsByClassName("platform-container")[0];
+var start = document.getElementsByClassName("platform-start")[0];
 var live = false; // This ensures game is active only after the player has hovered over the start platform.
 
 /************************************************************
@@ -12,7 +12,7 @@ var live = false; // This ensures game is active only after the player has hover
 /* Game starts when you hover over the start platform  */
 $(".platform-start").mouseenter(function(event) {
   live = true;
-  container.style.cursor = "crosshair";
+  start.style.border = "3px solid gold"
 });
 
 /* If you hover off of start platform not onto another platform, you lose  */
@@ -20,7 +20,6 @@ $(".platform-start").mouseleave(function(event) {
   if(!$(event.relatedTarget).hasClass("platform")) {
     explosion.style.display = "block";
     modalLose.style.display = "block";
-    container.style.cursor = "default";
   }
 });
 
@@ -29,6 +28,7 @@ Any unspecified classes will result in a loss */
 $(".platform").mouseleave(function(event) {
   if($(event.relatedTarget).hasClass("platform") ||
      $(event.relatedTarget).hasClass("platform-start") ||
+     $(event.relatedTarget).hasClass("level") ||
      $(event.relatedTarget).hasClass("friend") ||
      $(event.relatedTarget).hasClass("platform-win") ||
      $(event.relatedTarget).hasClass("power-up")) {
@@ -38,7 +38,6 @@ $(".platform").mouseleave(function(event) {
     if (live) {
       modalLose.style.display = "block";
       explosion.style.display = "block";
-      container.style.cursor = "default";
     }
   live = false;
   }
@@ -49,6 +48,5 @@ $(".platform-win").mouseenter(function() {
   if(live) {
       modalWin.style.display = "block";
       fireworks.style.display = "block";
-      container.style.cursor = "default";
 }
 });
